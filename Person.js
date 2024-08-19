c9db1345e90e88926a34ff99b99c28b32870aab4
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-19 06:36:57 -0400

    max name length

--- Person.js
+++ Person.js
@@ -1,9 +1,11 @@
+const MAX_NAME_LENGTH_SHOWN = 6;
+
 function Person({ age, hobbies, name }) {
   return (
     <div>
       <p>Learn some information about this person:</p>
       <ul>
-        <li>Name: {name.slice(0)}</li>
+        <li>Name: {name.slice(0, MAX_NAME_LENGTH_SHOWN)}</li>
         <li>Age: {age}</li>
       </ul>
       <ul>Hobbies:</ul>
