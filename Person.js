26879d908201d260c9d42e1e77cbe23881e2f7ac
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-19 06:35:44 -0400

    Person cont'd

--- Person.js
+++ Person.js
@@ -2,6 +2,10 @@ function Person({ age, hobbies, name }) {
   return (
     <div>
       <p>Learn some information about this person:</p>
+      <ul>
+        <li>Name: {name.slice(0)}</li>
+        <li>Age: {age}</li>
+      </ul>
       <ul>Hobbies:</ul>
     </div>
   );
