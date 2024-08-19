4489ab0da713a77046e1bb37f6ee6cab2c90fe61
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-19 06:42:28 -0400

    voteText

--- Person.js
+++ Person.js
@@ -1,6 +1,7 @@
 const MAX_NAME_LENGTH_SHOWN = 6;
 
 function Person({ age, hobbies, name }) {
+  const voteText = age >= 18 ? "Go vote!" : "Go study";
   const hobbiesLI = hobbies.map((hobby) => <li>{hobby}</li>);
   return (
     <div>
@@ -8,11 +9,12 @@ function Person({ age, hobbies, name }) {
       <ul>
         <li>Name: {name.slice(0, MAX_NAME_LENGTH_SHOWN)}</li>
         <li>Age: {age}</li>
+        <ul>
+          Hobbies:
+          {hobbiesLIs}
+        </ul>
       </ul>
-      <ul>
-        Hobbies:
-        {hobbiesLIs}
-      </ul>
+      <h3>{voteText}</h3>
     </div>
   );
 }
